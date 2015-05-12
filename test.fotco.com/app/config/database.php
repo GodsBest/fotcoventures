b8365cf502200a5ca6b78244967c73d0713bc36d
Peter Aganyo <GodsBest@users.noreply.github.com>
2015-05-12 13:31:31 -0400

    Add dB connection

--- test.fotco.com/app/config/database.php
+++ test.fotco.com/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'fotcoadm_products',
+			'username'  => 'fotcoadm',
+			'password'  => 'dJ9qm>gTsj1k',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

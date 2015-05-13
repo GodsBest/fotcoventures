03f655306d015a326c8c7dd676751ce2efb5c438
Peter Aganyo <GodsBest@users.noreply.github.com>
2015-05-13 12:48:30 -0400

    Update Product Order Process

---
+++ laravel/app/database/migrations/2015_05_13_152720_create_customer_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->string('email')->unique();
+			$table->string('phone');
+			$table->string('address');
+			$table->string('city');
+			$table->string('country');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customers');
+	}
+
+}
